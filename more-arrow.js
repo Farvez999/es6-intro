a2d3905d3588a7c51588f271fe1f34218e50b66b
Farvez Abdullah <engrfrzabdullah999@gmail.com>
2022-08-25 00:30:04 +0600

    More Arrow functions

---
+++ more-arrow.js
@@ -0,0 +1,20 @@
+// //arrow function
+// const add = (first, second) => first + second;
+// const result = add(10, 20);
+// //console.log(result);
+
+// //arrow function 2 more inout
+// const add = (first, second, third, four) => first + second + third + four;
+// const result1 = add(10, 20, 30, 40);
+// //console.log(result);
+
+// //arrow function no perameter
+// const addNoperameter = () => 3.14;
+
+// //arrow function one perametet
+// const addOnePerameter = (num) => num * 5;
+// console.log(addOnePerameter(10))
+
+//arrow function one perametet simple
+const addOnePerameter = num => num * 5;
+console.log(addOnePerameter(10))
